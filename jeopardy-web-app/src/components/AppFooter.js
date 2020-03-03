d52a05f7e730a5021c778421700bf402aeb73750
Ryan Fitch <rfitch@solstice.com>
2020-03-02 20:45:58 -0600

    Create initial components and pages

---
+++ jeopardy-web-app/src/components/AppFooter.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const AppFooter = () => <div>Footer</div>;
+
+export default AppFooter;
---
+++ jeopardy-web-app/src/components/AppHeader.js
@@ -0,0 +1,40 @@
+import React from "react";
+import Login20 from "@carbon/icons-react/lib/login/20";
+import {
+  Header,
+  HeaderName,
+  HeaderGlobalAction,
+  HeaderGlobalBar,
+  HeaderNavigation,
+  HeaderMenuItem
+} from "carbon-components-react/lib/components/UIShell";
+
+const AppHeader = () => (
+  <Header aria-label="Jeopardy Game">
+    <HeaderName id="header-name" href="/" prefix="">
+      Jeopardy
+    </HeaderName>
+    <HeaderNavigation aria-label="Jeopardy Game">
+      <HeaderMenuItem id="home-menu-item" href="/">
+        Home
+      </HeaderMenuItem>
+      <HeaderMenuItem id="about-menu-item" href="/about">
+        About
+      </HeaderMenuItem>
+      <HeaderMenuItem id="admin-menu-item" href="/admin">
+        Admin
+      </HeaderMenuItem>
+    </HeaderNavigation>
+    <HeaderGlobalBar>
+      <HeaderGlobalAction
+        id="login-button"
+        aria-label="Login"
+        onClick={() => {}}
+      >
+        <Login20 />
+      </HeaderGlobalAction>
+    </HeaderGlobalBar>
+  </Header>
+);
+
+export default AppHeader;
