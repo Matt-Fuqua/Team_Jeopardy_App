46691596fdf2588e64b65d2205b12b2e4c59abf3
Ryan Fitch <rfitch@solstice.com>
2020-03-07 12:57:09 -0600

    Add new actions for getting questions data

---
+++ jeopardy-web-app/src/actions/questions.js
@@ -0,0 +1,36 @@
+import store from '../store';
+import axios from '../axios/instance';
+
+export const getQuestionsStarted = () => {
+  return {
+    type: "GET_QUESTIONS_STARTED"
+  };
+};
+
+export const getQuestionsSuccess = post => {
+  return {
+    type: "GET_QUESTIONS_SUCCESS",
+    data: post
+  };
+};
+
+export const getQuestionsFailure = () => {
+  console.error('Unable to get questions data');
+  return {
+    type: "GET_QUESTIONS_FAILURE"
+  };
+};
+
+export const getQuestionsThunkAction = () => {
+  store.dispatch(getQuestionsStarted());
+  return (dispatch) => {
+    return axios.get(`/phase3`)
+      .then(response => {
+        dispatch(getQuestionsSuccess(response.data));
+      })
+      .catch(() => {
+        console.error('Unable to get questions data.');
+        dispatch(getQuestionsFailure());
+      });
+  }
+}
