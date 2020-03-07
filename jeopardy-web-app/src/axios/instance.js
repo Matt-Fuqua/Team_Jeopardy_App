03103f4a47f5f7c2cc0196958e39e0458bf0fadb
Ryan Fitch <rfitch@solstice.com>
2020-03-07 12:55:48 -0600

    Update axiod baseURL to reflect new API

--- jeopardy-web-app/src/axios/instance.js
+++ jeopardy-web-app/src/axios/instance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000'
+  baseURL: 'http://cs411teambfs.web.illinois.edu/rdb4'
 });
 
 export default instance;
