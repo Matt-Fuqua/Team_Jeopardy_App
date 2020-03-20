edf01e00e308d177269f3a76c438f72889f0080a
Ryan Fitch <rfitch@solstice.com>
2020-03-19 23:04:03 -0500

    Update combine reducers file

--- jeopardy-web-app/src/reducers/index.js
+++ jeopardy-web-app/src/reducers/index.js
@@ -1,4 +1,6 @@
 import { combineReducers } from "redux";
+import login from './login';
 import questions from './questions';
+import registerUser from './registerUser';
 
-export default combineReducers({ questions });
+export default combineReducers({ login, questions, registerUser });
