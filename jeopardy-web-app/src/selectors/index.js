88d8a8bd66f4a50a775fe99f1d29a4da51d63a6e
Ryan Fitch <rfitch@solstice.com>
2020-03-19 23:06:05 -0500

    Create selectors to reflect/support new functionality

--- jeopardy-web-app/src/selectors/index.js
+++ jeopardy-web-app/src/selectors/index.js
@@ -1,5 +1,15 @@
 import { get } from "lodash";
 
+// Login Selectors
+export const loginStatus = state => get(state.login, "loginStatus");
+export const loginIsFetching = state => get(state.login, "isFetching");
+export const loginIsError = state => get(state.login, "isError");
+
+// Register User Selectors
+export const registerStatus = state => get(state.registerUser, "registerStatus");
+export const registerIsFetching = state => get(state.registerUser, "isFetching");
+export const registerIsError = state => get(state.registerUser, "isError");
+
 // Questions Selectors
 export const getQuestionsData = state => get(state.questions, "questionsData");
 export const getQuestionsIsFetching = state => get(state.questions, "isFetching");
