62764e653eb7389c44a6d0ff2d3c0c4b1d102e3b
Ryan Fitch <rfitch@solstice.com>
2020-03-30 22:47:37 -0500

    Export newGame reducer

--- jeopardy-web-app/src/reducers/index.js
+++ jeopardy-web-app/src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from "redux";
 import login from './login';
+import newGame from './newGame';
 import registerUser from './registerUser';
 
-export default combineReducers({ login, registerUser });
+export default combineReducers({ login, newGame, registerUser });
