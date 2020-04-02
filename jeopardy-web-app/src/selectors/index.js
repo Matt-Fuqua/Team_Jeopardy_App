ce322defcdcdd19edf6a9d8fd4bdf41c1a7518f7
Ryan Fitch <rfitch@solstice.com>
2020-04-02 00:08:52 -0500

    Resolve issues with the New Game selectors

--- jeopardy-web-app/src/selectors/index.js
+++ jeopardy-web-app/src/selectors/index.js
@@ -6,14 +6,14 @@ export const loginIsFetching = state => get(state.login, "isFetching");
 export const loginIsError = state => get(state.login, "isError");
 
 // New Game Selectors
-export const newGameId = state => get(state.newGame.gameId);
-export const newGameCategories = state => get(state.newGame.categories);
-export const newGameFinal = state => get(state.newGame.final);
-export const newGameQuestions = state => get(state.newGame.questions);
-export const newGameRoundOne = state => get(state.newGame.roundOne);
-export const newGameRoundTwo = state => get(state.newGame.roundTwo);
-export const newGameIsFetching = state => get(state.newGame.isFetching);
-export const newGameIsError = state => get(state.newGame.isError);
+export const newGameId = state => get(state.newGame, "gameId");
+export const newGameCategories = state => get(state.newGame, "categories");
+export const newGameFinal = state => get(state.newGame, "final");
+export const newGameQuestions = state => get(state.newGame, "questions");
+export const newGameRoundOne = state => get(state.newGame, "roundOne");
+export const newGameRoundTwo = state => get(state.newGame, "roundTwo");
+export const newGameIsFetching = state => get(state.newGame, "isFetching");
+export const newGameIsError = state => get(state.newGame, "isError");
 
 // Register User Selectors
 export const registerStatus = state => get(state.registerUser, "registerStatus");
