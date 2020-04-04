fb96129ed57f2c7f00a3775b87659347f99ec08b
Matt F <mfuqua2@illinois.edu>
2020-04-03 18:38:37 -0700

    Cleaned up home page and added logo

--- jeopardy-web-app/src/pages/Home.js
+++ jeopardy-web-app/src/pages/Home.js
@@ -1,31 +1,47 @@
 import React from 'react';
 import { Column, Grid, Row } from 'carbon-components-react';
 import { NewUser, UserLogin } from '../components';
+import JeopardyLogo from "../images/homePageImage.jpg";
 
 const Home = () => {
   return (
   
      <div>
         <Grid>
-          <Row style={{ padding: "50px" }}>
-            <Column>
-              <h1 style={{ color: "blue" }}> Welcome to online Jeopardy </h1><br></br>
-              <p style={{ fontSize: "30px" }}> 
-                If you are a returning player, please login. <br></br><br></br>
-                If this is your first time, you will need to create a user profile. <br></br><br></br>
-                This will allow you to track your progress and stats.
-              </p>   
-            </Column>
-            <Column>
+          <Row>
+          <Column style={{ font: "5px" }}>
+            <Row style={{ padding: "10px" }}>
+           
+              <h1 style={{ color: "blue" }}> Welcome to online Jeopardy </h1>
+            </Row>
+            <Row>
+              <p> 
+                If you are a returning player, please login. 
+              </p>
+            </Row>
+            <Row>
+              <p>
+                If this is your first time, you will need to create a user profile.
+              </p>
+              </Row>
+              <Row>
+                <p>
+                  This will allow you to track your progress and stats.
+                </p>
+              </Row> 
+              <Row style={{ padding: "20px" }}>
+                <img alt="Jeopardy Logo" src={JeopardyLogo} style={{ height: 300, width: 500 }} />
+              </Row>             
+          </Column>
+
+          <Column>
+            <Row style={{ padding: "10px" }}>
               <UserLogin />
-            </Column>
-          </Row>
-          <Row style={{ padding: "10px" }}>
-            <Column>
-            </Column>
-            <Column>
+            </Row>
+            <Row style={{ padding: "10px" }}>
               <NewUser />
-            </Column>
+            </Row>
+          </Column>
           </Row>
         </Grid>
       </div>
