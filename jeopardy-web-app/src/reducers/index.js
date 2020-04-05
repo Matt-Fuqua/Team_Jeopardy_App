92b09be9d1da22d87ae1b3d13126275b295752d7
Ryan Fitch <rfitch@solstice.com>
2020-04-04 23:36:46 -0500

    Create question display reducer

--- jeopardy-web-app/src/reducers/index.js
+++ jeopardy-web-app/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import login from './login';
 import newGame from './newGame';
+import questionDisplay from './questionDisplay';
 import registerUser from './registerUser';
 
-export default combineReducers({ login, newGame, registerUser });
+export default combineReducers({ login, newGame, questionDisplay, registerUser });
---
+++ jeopardy-web-app/src/reducers/questionDisplay.js
@@ -0,0 +1,31 @@
+const initialData = {
+  open: false,
+  question: "",
+  questionId: "",
+  value: ""
+};
+
+const questionDisplayReducer = (state = initialData, action) => {
+  switch (action.type) {
+    case "DISPLAY_QUESTION_MODAL":
+      return {
+        ...state,
+        open: true,
+        question: action.data.question,
+        questionId: action.data.questionId,
+        value: action.data.questionValue
+      };
+    case "CLOSE_QUESTION_MODAL":
+      return {
+        ...state,
+        open: false,
+        question: "",
+        questionId: "",
+        value: ""
+      };
+    default:
+      return state;
+  }
+};
+
+export default questionDisplayReducer;
