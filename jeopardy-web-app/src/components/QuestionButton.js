1f3e39672323fa5877e4c7687c48d2c1a4932b69
Ryan Fitch <rfitch@solstice.com>
2020-04-04 23:34:54 -0500

    Update Question Button functionality to include question modal direction

--- jeopardy-web-app/src/components/QuestionButton.js
+++ jeopardy-web-app/src/components/QuestionButton.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
+import { displayQuestionModal } from '../actions/questionDisplay';
 
 const QuestionButton = props => {
   const buttonColor = props.enabled ? '#0353e9' : '#F5F5F5';
+
+  const dispatch = useDispatch();
+  const presentModal = () => {
+    dispatch(displayQuestionModal({ questionId: props.questionId, question: props.question, value: props.value }));
+  }
 
   return (
     <button
       style={{ backgroundColor: buttonColor, color: '#FFD700', fontSize: 'medium', fontWeight: 'bold', height: 90, width: 150 }}
       type="button" 
-      onClick={() => console.log(props.questionId)}
+      onClick={() => presentModal()}
     >
       {props.value}
     </button>
@@ -17,6 +24,7 @@ const QuestionButton = props => {
 
 QuestionButton.propTypes = {
   enabled: PropTypes.bool,
+  question: PropTypes.string,
   questionId: PropTypes.string,
   value: PropTypes.string
 };
