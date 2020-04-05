daa1d57964060cacbcd300e91cd73be2094001f8
Ryan Fitch <rfitch@solstice.com>
2020-04-04 23:34:10 -0500

    Map round one questions to buttons

--- jeopardy-web-app/src/components/Gameboard.js
+++ jeopardy-web-app/src/components/Gameboard.js
@@ -1,155 +1,32 @@
 
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Column, Grid, Row } from 'carbon-components-react';
-import QuestionButton from './QuestionButton';
+import { Grid } from 'carbon-components-react';
+
 import {
-    newGameQuestions
+    newGameRoundOne
 } from '../selectors';
+import QuestionButton from './QuestionButton';
 
 const Gameboard = () => {
-    const questions = useSelector(newGameQuestions);
-    
+    const roundOneQuestions = useSelector(newGameRoundOne);
+
     let buttons = [];
 
-    Object.values(questions).forEach(object => {
-        Object.values(object).forEach(question => {
-            let questionValue = "";
-            if(question.value === null) {
-                questionValue = "0";
-            } else {
-                questionValue = question.value.toString();
-            }
-            buttons.push(<QuestionButton enabled={true} value={questionValue} questionId={question.question_id.toString()} />);
+    if(roundOneQuestions && roundOneQuestions.length) {
+        roundOneQuestions.forEach(element => {
+            buttons.push(<h3>{element.category}</h3>);
+            const questions = element.questions;
+            questions.forEach(item => {
+                buttons.push(<QuestionButton enabled={true} value={item.value.toString()} question={item.question.toString()} questionId={item.question_id.toString()} />);
+            });
         });
-    })
+    }
 
     return (
         <div>
             <Grid style = {{ padding: "100px" }}>
                 {buttons}
-                {/* <Row>
-                    <Column>  
-                        <Row>
-                            Cat 1
-                        </Row>
-                        <Row>
-                            <QuestionButton enabled={true} value= { buttonValue1 } />
-                        </Row>
-                        <Row>
-                            <QuestionButton enabled={true} value= { buttonValue2 } />
-                        </Row>
-                        <Row>
-                            <QuestionButton enabled={true} value= { buttonValue3 } />
-                        </Row>
-                        <Row>
-                            <QuestionButton enabled={true} value= { buttonValue4 } />
-                        </Row>
-                        <Row>
-                            <QuestionButton enabled={true} value= { buttonValue5 } />   
-                        </Row>
-                    </Column>
-                    <Column>  
-                        <Row>
-                            Cat 2
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />   
-                        </Row>
-                    </Column>
-                    <Column>  
-                        <Row>
-                            Cat 3
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />   
-                        </Row>
-                    </Column>
-                    <Column>  
-                        <Row>
-                            Cat 4
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />   
-                        </Row>
-                    </Column>
-                    <Column>  
-                        <Row>
-                            Cat 5
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />   
-                        </Row>
-                    </Column>
-                    <Column>  
-                        <Row>
-                            Cat 6
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />
-                        </Row>
-                        <Row>
-                            <QuestionButton />   
-                        </Row>
-                    </Column>
-                </Row> */}
             </Grid>
         </div>
     );
