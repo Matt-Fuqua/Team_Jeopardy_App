e3b99c16e1448fcc25c316561c100678b09c81ab
Ryan Fitch <rfitch@solstice.com>
2020-04-04 23:37:20 -0500

    Update new game selectors and create question display selectors

--- jeopardy-web-app/src/selectors/index.js
+++ jeopardy-web-app/src/selectors/index.js
@@ -7,13 +7,17 @@ export const loginIsError = state => get(state.login, "isError");
 
 // New Game Selectors
 export const newGameId = state => get(state.newGame, "gameId");
-export const newGameCategories = state => get(state.newGame, "categories");
-export const newGameFinal = state => get(state.newGame, "final");
-export const newGameQuestions = state => get(state.newGame, "questions");
 export const newGameRoundOne = state => get(state.newGame, "roundOne");
 export const newGameRoundTwo = state => get(state.newGame, "roundTwo");
+export const newGameFinal = state => get(state.newGame, "final");
 export const newGameIsFetching = state => get(state.newGame, "isFetching");
 export const newGameIsError = state => get(state.newGame, "isError");
+
+// Question Display Selectors
+export const questionDisplayOpen = state => get(state.questionDisplay, "open");
+export const questionDisplayQuestion = state => get(state.questionDisplay, "question");
+export const questionDisplayQuestionId = state => get(state.questionDisplay, "questionId");
+export const questionDisplayValue = state => get(state.questionDisplay, "value");
 
 // Register User Selectors
 export const registerStatus = state => get(state.registerUser, "registerStatus");
