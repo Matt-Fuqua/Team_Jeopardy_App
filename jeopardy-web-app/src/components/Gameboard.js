63477e87c020bf7071b44d2e247a6741942c8f11
Ryan Fitch <rfitch@solstice.com>
2020-04-25 22:45:50 -0500

    Enahance the way the questions data is being mapped and displayed

--- jeopardy-web-app/src/components/Gameboard.js
+++ jeopardy-web-app/src/components/Gameboard.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Grid } from 'carbon-components-react';
+import { Column, Grid, Row } from 'carbon-components-react';
 
 import {
     newGameRoundOne
@@ -11,22 +11,72 @@ import QuestionButton from './QuestionButton';
 const Gameboard = () => {
     const roundOneQuestions = useSelector(newGameRoundOne);
 
-    let buttons = [];
+    let buttonsList = {
+        categoryOne: [],
+        categoryTwo: [],
+        categoryThree: [],
+        categoryFour: [],
+        categoryFive: [],
+        categorySix: []
+    };
+
+    const mapIndexToArrayName = index => {
+        switch(index) {
+            case 1:
+                return buttonsList.categoryOne;
+            case 2:
+                return buttonsList.categoryTwo;
+            case 3:
+                return buttonsList.categoryThree;
+            case 4:
+                return buttonsList.categoryFour;
+            case 5:
+                return buttonsList.categoryFive;
+            case 6:
+                return buttonsList.categorySix;
+            default:
+                return buttonsList.categorySix;
+        }
+    };
 
     if(roundOneQuestions && roundOneQuestions.length) {
-        roundOneQuestions.forEach(element => {
-            buttons.push(<h3>{element.category}</h3>);
+        roundOneQuestions.forEach((element, index) => {
+            mapIndexToArrayName(index+1).push(<p>{element.category}</p>);
             const questions = element.questions;
             questions.forEach(item => {
-                buttons.push(<QuestionButton enabled={true} value={item.value.toString()} question={item.question.toString()} questionId={item.question_id.toString()} />);
+                mapIndexToArrayName(index+1).push(<QuestionButton enabled={true} value={item.value.toString()} question={item.question.toString()} questionId={item.question_id.toString()} />);
+                mapIndexToArrayName(index+1).push(<br />);
             });
+            console.log(index+1);
         });
     }
 
     return (
-        <div>
-            <Grid style = {{ padding: "100px" }}>
-                {buttons}
+        <div className="bx--col" style={{ display: 'flex', flexDirection: 'column', padding: 0}}>
+            <Grid className="bx--col" style={{paddingRight: 0, paddingLeft: 0}}>
+                <Row className="bx--col">
+                    <Column>
+                        {buttonsList.categoryOne}
+                    </Column>
+                    <Column>
+                        {buttonsList.categoryTwo}
+                    </Column>
+                    <Column>
+                        {buttonsList.categoryThree}
+                    </Column>
+                    <Column>
+                        {buttonsList.categoryFour}
+                    </Column>
+                    <Column>
+                        {buttonsList.categoryFour}
+                    </Column>
+                    <Column>
+                        {buttonsList.categoryFive}
+                    </Column>
+                    <Column>
+                        {buttonsList.categorySix}                
+                    </Column>
+                </Row>
             </Grid>
         </div>
     );
