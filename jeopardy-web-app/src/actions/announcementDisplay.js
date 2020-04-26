e9cb5386b77947140e519bbaa8b5e8a411d069e5
Ryan Fitch <rfitch@solstice.com>
2020-04-25 22:44:35 -0500

    Create Announcement Display actions

---
+++ jeopardy-web-app/src/actions/announcementDisplay.js
@@ -0,0 +1,11 @@
+export const displayAnnouncementModal = () => {
+  return {
+    type: "DISPLAY_ANNOUNCEMENT_MODAL"
+  };
+};
+
+export const closeAnnouncementModal = () => {
+  return {
+    type: "CLOSE_ANNOUNCEMENT_MODAL"
+  };
+};
