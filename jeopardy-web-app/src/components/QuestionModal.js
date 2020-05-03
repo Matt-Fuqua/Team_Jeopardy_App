b937295e48562049e5d23fb9486fb014273e3440
Matt F <mfuqua2@illinois.edu>
2020-05-03 16:27:03 -0700

    able to send answer to be checked. Timer is work in progress.

--- jeopardy-web-app/src/components/QuestionModal.js
+++ jeopardy-web-app/src/components/QuestionModal.js
@@ -1,25 +1,59 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Form, FormGroup, Modal, TextInput } from 'carbon-components-react';
+import { Column, Grid, Row } from 'carbon-components-react';
 
+import { checkAnswerThunkAction } from '../actions/checkAnswer';
 import { closeQuestionModal } from '../actions/questionDisplay';
-import { questionDisplayOpen, questionDisplayQuestion, /* questionDisplayQuestionId, questionDisplayValue */ } from '../selectors/index';
+import { newGameId, questionDisplayOpen, questionDisplayQuestion, questionDisplayQuestionId, /*questionDisplayValue */ } from '../selectors/index';
+import { initialTimerDuration, initialTimerEnabled }  from '../selectors/index';
+import { correctAnswer, isAnswerCorrect } from '../selectors/index';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+// https://www.npmjs.com/package/react-countdown-circle-timer
+// yarn add react-countdown-circle-timer
+// props: https://github.com/vydimitrov/react-countdown-circle-timer#props-for-both-reactreact-native
 
+ 
 const QuestionModal = () => {
   const dispatch = useDispatch();
-
+  const gameId = useSelector(newGameId);
   const modalOpen = useSelector(questionDisplayOpen);
   const modalQuestion = useSelector(questionDisplayQuestion);
-  // const modalQuestionId = useSelector(questionDisplayQuestionId);
+  const initialTimerActive = useSelector(initialTimerEnabled);
+  const initialTimerLength = useSelector(initialTimerDuration);
+  const modalQuestionId = useSelector(questionDisplayQuestionId);
+
+  const actualAnswer = useSelector(correctAnswer);
+  const isCorrect = useSelector(isAnswerCorrect);
+
   // const modalQuestionValue = useSelector(questionDisplayValue);
 
+  var hideSetting = { visibility:"visible" }
+  var contestantId = 1000;
   const [answerInput, setAnswerInput] = useState();
 
   const handleFormSubmit = e => {
+    console.log("submit button event");
+    console.log("game id: " + gameId);
+    console.log("question ID: " + modalQuestionId);
+    console.log("contestant ID is: " + contestantId);
+    console.log("answer given: " + answerInput);
     e.preventDefault();
-    // dispatch action - submit answer to api middle layer
+    dispatch(checkAnswerThunkAction(gameId, modalQuestionId, contestantId ,answerInput));
     setAnswerInput("");
   }
+
+  const handleFormGuess = e => {
+    console.log("guess button event");
+    e.preventDefault();
+    hideSetting =  { visibility:"visible" };
+  }
+
+  const guessTimeUp = e => {
+    console.log("time up");
+
+  }
+
 
   return (
     <Modal
@@ -28,8 +62,8 @@ const QuestionModal = () => {
       modalAriaLabel="A label to be read by screen readers on the modal root node"
       modalHeading={modalQuestion}
       onRequestClose={() => dispatch(closeQuestionModal())}
-      onRequestSubmit={() => console.log('on request submit')}
-      onSecondarySubmit={() => console.log('on secondary input')}
+      onRequestSubmit={ handleFormSubmit }      
+      onSecondarySubmit={ handleFormGuess }
       open={modalOpen}
       passiveModal={false}
       primaryButtonDisabled={false}
@@ -37,10 +71,10 @@ const QuestionModal = () => {
       secondaryButtonText="Guess"
       selectorPrimaryFocus="[data-modal-primary-focus]"
     >
-      <Form
-        onSubmit={handleFormSubmit}
+      <Form style ={ hideSetting }
+       // onSubmit={handleFormSubmit}
       >
-        <FormGroup
+        <FormGroup 
           invalid={false}
           message={false}
           legendText=""
@@ -57,6 +91,20 @@ const QuestionModal = () => {
           />
         </FormGroup>
       </Form>
+      <Grid>
+        <Row>
+          <Column>
+            <CountdownCircleTimer
+              isPlaying = {initialTimerActive}
+              duration={initialTimerLength}
+              colors={[['#004777', 0.33], ['#F7B801', 0.33], ['#A30000']]}
+            >
+              {({ remainingTime }) => remainingTime}
+            </CountdownCircleTimer>
+          </Column>
+   
+        </Row>
+      </Grid>
     </Modal>
   );
 };
