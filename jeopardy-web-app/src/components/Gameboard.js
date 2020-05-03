77175bf6ff795172edfb8186797c20a6c36ddefc
Matt F <mfuqua2@illinois.edu>
2020-05-03 16:25:56 -0700

    adjusted style of gameboard

--- jeopardy-web-app/src/components/Gameboard.js
+++ jeopardy-web-app/src/components/Gameboard.js
@@ -2,15 +2,11 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Column, Grid, Row } from 'carbon-components-react';
-
-import {
-    newGameRoundOne
-} from '../selectors';
+import { newGameRoundOne } from '../selectors';
 import QuestionButton from './QuestionButton';
 
 const Gameboard = () => {
     const roundOneQuestions = useSelector(newGameRoundOne);
-
     let buttonsList = {
         categoryOne: [],
         categoryTwo: [],
@@ -41,31 +37,27 @@ const Gameboard = () => {
 
     if(roundOneQuestions && roundOneQuestions.length) {
         roundOneQuestions.forEach((element, index) => {
-            mapIndexToArrayName(index+1).push(<p>{element.category}</p>);
+            mapIndexToArrayName(index+1).push(<div style={{ height: 75 }}><p style={{fontSize: "20px"}}>{element.category}</p></div>);
             const questions = element.questions;
             questions.forEach(item => {
                 mapIndexToArrayName(index+1).push(<QuestionButton enabled={true} value={item.value.toString()} question={item.question.toString()} questionId={item.question_id.toString()} />);
                 mapIndexToArrayName(index+1).push(<br />);
             });
-            console.log(index+1);
         });
     }
 
     return (
-        <div className="bx--col" style={{ display: 'flex', flexDirection: 'column', padding: 0}}>
-            <Grid className="bx--col" style={{paddingRight: 0, paddingLeft: 0}}>
-                <Row className="bx--col">
+        <div style={{ width: 1050 }}>    
+            <Grid className="bx--col" style={{paddingRight: "0px", paddingLeft: "0px"}}>
+                <Row className="bx--col" >
                     <Column>
                         {buttonsList.categoryOne}
-                    </Column>
+                    </Column  >
                     <Column>
                         {buttonsList.categoryTwo}
                     </Column>
                     <Column>
                         {buttonsList.categoryThree}
-                    </Column>
-                    <Column>
-                        {buttonsList.categoryFour}
                     </Column>
                     <Column>
                         {buttonsList.categoryFour}
