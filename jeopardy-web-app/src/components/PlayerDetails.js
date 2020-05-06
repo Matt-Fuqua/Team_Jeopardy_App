29bd946ef503709446a0f3de618ba0bd43b2059a
Ryan Fitch <rfitch@solstice.com>
2020-05-05 23:37:18 -0500

    Update Player Details feature

--- jeopardy-web-app/src/components/PlayerDetails.js
+++ jeopardy-web-app/src/components/PlayerDetails.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const PlayerDetails = () => {
+const PlayerDetails = props => {
   return (
-    <h1> Player </h1>
+    <div>
+      <h3 style={{ fontWeight: 'bold' }}>{props.name}</h3>
+      <h3>Score: {props.score}</h3>
+    </div>
   );
+};
 
+PlayerDetails.propTypes = {
+  name: PropTypes.string,
+  score: PropTypes.number
 };
 
 export default PlayerDetails;
