d65bf9e5b41e0c0c83502dc386c4c4ac8554989c
Ryan Fitch <rfitch@solstice.com>
2020-05-06 23:15:27 -0500

    Create new component for end of game animation

---
+++ jeopardy-web-app/src/components/EndGameAnimation.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Fireworks from 'fireworks/lib/react';
+import { Modal } from 'carbon-components-react';
+
+const EndGameAnimation = props => {
+  const visibilityStatus = props.visibility ? "visible" : "hidden";
+  const winnerMessage = props.winnerStatus ? "Congrats Winner & Thank you for playing!" : "Try again & Thank you for playing!";
+  const fxProps = {
+    count:  3,
+    interval: 200,
+    colors: ['#4c2491', '#1815bf', '#d117e6'],
+    calc: (props, i) => ({
+      ...props,
+      x: (i + 1) * (window.innerWidth / 3) - (i + 1) * 100,
+      y: 200 + Math.random() * 100 - 50 + (i === 2 ? -80 : 0)
+    })
+  }
+  return(
+    <div style={{ visibility: visibilityStatus }}>
+      <Fireworks {...fxProps} />
+      <Modal
+        iconDescription="End Game Modal"
+        modalAriaLabel="End Game Modal"
+        modalHeading={winnerMessage}
+        onRequestClose={() => {}}
+        onRequestSubmit={ ()=>{} }      
+        onSecondarySubmit={  ()=>{}  }
+        open={props.visibility}
+        passiveModal={false}
+        primaryButtonText=""
+        secondaryButtonText=""
+      />
+    </div>
+  );
+};
+
+EndGameAnimation.propTypes = {
+  visibility: PropTypes.bool,
+  winnerStatus: PropTypes.bool
+};
+
+export default EndGameAnimation;
