39b2addb1c6c0130c191a2b5ecd37cea79908888
Ryan Fitch <rfitch@solstice.com>
2020-05-06 23:14:42 -0500

    Add new route for Dashboard component

--- jeopardy-web-app/src/App.js
+++ jeopardy-web-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { AppHeader } from './components';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { About, Admin, GamePlay, Home } from './pages';
+import { About, Admin, Dashboard, GamePlay, Home } from './pages';
 import { Content } from "carbon-components-react/lib/components/UIShell";
 
 function App() {
@@ -17,6 +17,9 @@ function App() {
             </Route>
             <Route path="/admin">
               <Admin />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
             </Route>
             <Route path="/gamePlay">
               <GamePlay />
