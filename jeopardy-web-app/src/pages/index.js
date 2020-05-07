950e0e6482f52faab97e559b36e1cffc563a8e38
Ryan Fitch <rfitch@solstice.com>
2020-05-06 23:16:38 -0500

    Create a frame for dashboard

--- jeopardy-web-app/src/pages/index.js
+++ jeopardy-web-app/src/pages/index.js
@@ -1,5 +1,6 @@
 export { default as About } from './About';
 export { default as Admin } from './Admin';
+export { default as Dashboard } from './Dashboard';
 export { default as GameOver } from './GameOver';
 export { default as GameSelection } from './GameSelection';
 export { default as Home } from './Home';
